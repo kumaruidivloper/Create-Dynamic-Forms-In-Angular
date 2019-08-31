9bd9f8bc0c99cf98a0e0b597f506712b8ed9c738
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-31 13:58:25 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Create-Dynamic-Forms-In-Angular';
+export class AppComponent implements OnInit {
+  public title = 'Create-Dynamic-Forms-In-Angular';
+
+  form: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      firstname: new FormControl('Kumar')
+    });
+  }
 }
