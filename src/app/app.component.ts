bde1f6cd2b41a2f796e4132bc1c4978596626b2c
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-31 14:04:36 +0530

    Change-Firstname-To-Age-Manually

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      firstname: new FormControl('Kumar')
+      age: new FormControl('32')
     });
   }
 }
