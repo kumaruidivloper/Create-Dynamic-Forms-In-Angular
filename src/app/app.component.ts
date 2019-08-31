2a19b7a9710e7a794cce6e0c2db4ee1ae197296e
kumaruidivloper <kumaruidivloper@gmail.com>
2019-08-31 17:31:12 +0530

    Dynamically-Create-Multiple-FormControls-From-Data-Set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,30 @@ export class AppComponent implements OnInit {
   public title = 'Create-Dynamic-Forms-In-Angular';
 
   public form: FormGroup;
-  public fieldLable = 'Age';
-  public fieldName = 'age';
-  public fieldValue = '30';
+  // public fieldLable = 'Age';
+  // public fieldName = 'age';
+  // public fieldValue = '30';
+
+  // More number of Fields
+  public person = {
+    firstname: 'Kumar',
+    surname: 'shan',
+    age: 30,
+    twitter: '@KumarShan'
+  };
+
+  personProps = [];
 
   ngOnInit() {
-    this.form = new FormGroup({
-      [this.fieldName]: new FormControl(this.fieldValue)
-    });
+    const formDataObj = {};
+    for (const prop of Object.keys(this.person)) {
+        formDataObj[prop] = new FormControl(this.person[prop]);
+        this.personProps.push(prop);
+    }
+    // this.form = new FormGroup({
+    //   [this.fieldName]: new FormControl(this.fieldValue)
+    // });
+
+    this.form = new FormGroup(formDataObj);
   }
 }
